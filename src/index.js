ef0255388906accc1e8a33e3b258f8d8c5d37b2f
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-02 22:40:31 -0300

    initial configs index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(authRouter);
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server running in port ${port}`));
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-
+import { validateSignUp } from "../middlewares/signUp.middlware";
 const authRouter = Router();
 
-authRouter.post("signUp", SignUp);
+authRouter.post("signUp", validateSignUp, SignUp);
 authRouter.post("signIn", SignIn);
-
