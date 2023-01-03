c3a330889411a863430b6f3f5aa9a7ed5e651668
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-02 22:22:40 -0300

    auth middleware

--- src/models/signUp..schema.js
+++ src/models/signUp..schema.js
@@ -1,8 +1,10 @@
 import joi from "joi";
 
-export const signUpSchema = joi.object({
-  email: joi.string().required().min(3).max(50),
+const signUpSchema = joi.object({
+  email: joi.string().email().required().min(3).max(50),
   password: joi.string().required().min(6),
   username: joi.string().required().min(6).max(50),
   pictureUrl: joi.string().required().min(10),
 });
+
+export default signUpSchema;
