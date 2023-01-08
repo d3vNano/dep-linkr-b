cc237de8df2d5881d7a35b28765456a414e42358
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-07 22:46:35 -0300

    Hashtags

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
 import usersRouter from "./routes/user.routes.js";
+import hashtagsRouter from "./routes/hashtags.routes.js";
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.use(authRouter);
 app.use(usersRouter);
+app.use(hashtagsRouter);
 
 const port = process.env.PORT;
 
--- src/middlewares/signIn.middleware.js
+++ src/middlewares/signIn.middleware.js
@@ -1,37 +1,38 @@
+
 import { connectionDB } from "../database/db.js";
 import signInSchema from "../models/signIn.schema.js";
 import validateUser from "../repository/validate.signIn.repository.js";
 import bcrypt from 'bcrypt';
 
 export async function validateSignIn(req,res,next) {
-  const { email, password} = req.body;
+const { email, password} = req.body;
 
-  try{
-  if(!email || !password){
-    return res.status(422).send({message: "Por favor, preencha todos os campos corretamente!"})
-  }
-  const userExists = await validateUser(email);
+try{
+if(!email || !password){
+return res.status(422).send({message: "Por favor, preencha todos os campos corretamente!"})
+}
+const userExists = await validateUser(email);
+if (userExists.rows.length === 0) {
+return res.status(409).send({message:"Usuário ou senha Incorreta!"});
+}
+console.log(password, userExists.rows[0].password);
+const passwordOk = bcrypt.compareSync(
+password,
+userExists.rows[0].password
+);
+if (!passwordOk ) {
+return res.status(409).send({message:"Usuário ou senha Incorreta!"});
+}
+const { error } = signInSchema.validate(req.body, { abortEarly: false });
+if (error) {
+const errors = error.details.map((detail) => detail.message);
+return res.status(422).send({ errors });
+}
 
-  const passwordOk = bcrypt.compareSync(
-    password,
-    userExists.rows[0].password
-    );
-
-    if (!passwordOk ) {
-      return res.status(409).send({message:"Usuário ou senha Incorreta!"});
-    }
-  const { error } = signInSchema.validate(req.body, { abortEarly: false });
-  if (error) {
-    const errors = error.details.map((detail) => detail.message);
-    return res.status(422).send({ errors });
-  }
-   
-
-
-  }catch (err) {
-    console.log(err);
-    return res.sendStatus(422);
-  }
-  res.locals.user = {email, password}
-  next();
+}catch (err) {
+console.log(err);
+return res.sendStatus(422);
+}
+res.locals.user = {email, password}
+next();
 }
