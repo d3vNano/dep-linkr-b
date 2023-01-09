0c6b8e23c628fd18879651e7bef53f6e6acd9714
dorigoluiz <luizdorigo01@gmail.com>
2023-01-09 17:42:46 -0300

    feat: post-createPost & add user_id response

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,14 +1,20 @@
 import postsRepositories from "../repository/posts.repository.js";
 
-async function postList(req,res){
+async function createPost(req, res) {
+    const { link, description, user_id } = req.body;
+
+    res.status(201).send("New post created!");
+}
+
+async function postList(req, res) {
     try {
         const listPosts = postsRepositories.AllPosts();
 
-        res.status(200).send((await listPosts).rows)
+        res.status(200).send((await listPosts).rows);
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.sendStatus(500);
     }
-};
+}
 
-export {postList};
+export { postList, createPost };
--- src/controllers/signIn.controllers.js
+++ src/controllers/signIn.controllers.js
@@ -1,22 +1,28 @@
-import {v4 as uuidV4} from 'uuid';
-import validateUser from '../repository/validate.signIn.repository.js';
-import validatesSession from '../repository/validate.sessions.repository.js';
+import { v4 as uuidV4 } from "uuid";
+import validateUser from "../repository/validate.signIn.repository.js";
+import validatesSession from "../repository/validate.sessions.repository.js";
 
-export default async function signIn(req,res){
-   const {email, password}= res.locals.user;
+export default async function signIn(req, res) {
+    const { email, password } = res.locals.user;
 
-   const token = uuidV4();
+    const token = uuidV4();
 
-   try {
-    const userExists = await validateUser(email);
- 
-    await validatesSession(token, userExists);
-  
-  const user = userExists.rows[0]
-   return res.status(200).send({ token, username: user.username, picture_url: user.picture_url});
-  } catch (err) {
-    console.log(err);
-    return res.sendStatus(422);
-  }
+    try {
+        const userExists = await validateUser(email);
 
-  }
+        await validatesSession(token, userExists);
+
+        const user = userExists.rows[0];
+        return res
+            .status(200)
+            .send({
+                token,
+                username: user.username,
+                picture_url: user.picture_url,
+                user_id: user.id,
+            });
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(422);
+    }
+}
