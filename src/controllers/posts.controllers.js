2b978626f2f389042a34ae449ca1df23d9d25cab
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-09 17:17:39 -0300

    metadata

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,13 +1,33 @@
 import postsRepositories from "../repository/posts.repository.js";
-
+import urlMetadata from "url-metadata";
+import { connectionDB } from "../database/db.js";
 async function postList(req,res){
     try {
-        const listPosts = postsRepositories.AllPosts();
 
-        res.status(200).send((await listPosts).rows)
+const link = await connectionDB.query("Select link from posts");
+const arr = [];
+for(let i = 0; i< link.rows.length; i++){
+    
+    const metadata = await urlMetadata(`http://${link.rows[i].link}`, {descriptionLength: 110 });
+    const { url, title, description, image } = metadata;
+    arr.push({metaUrl:url, metaTitle:title, metaDescription:description, metaImage:image})
+    
+    
+}
+const response =[];
+const listPosts = await postsRepositories.AllPosts();
+for(let i = 0; i< listPosts.rows.length ; i++){
+response.push({...listPosts.rows[i],...arr[i]});
+
+}
+
+console.log(response);
+
+
+       return res.status(200).send(response)
     } catch (error) {
         console.log(error)
-        res.sendStatus(500);
+       return res.sendStatus(500);
     }
 };
 
