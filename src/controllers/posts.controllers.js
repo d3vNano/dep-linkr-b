7dfa568723fa200b65ff175288035bb720c3b4bf
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-10 19:27:37 -0300

    add or remove like

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -6,12 +6,16 @@ async function postList(req,res){
 
 const link = await connectionDB.query("Select link from posts");
 const arr = [];
+
+const like = await connectionDB.query("select * from likes_info");
+
+console.log(like.rows, "like");
+
 for(let i = 0; i< link.rows.length; i++){
     
     const metadata = await urlMetadata(`http://${link.rows[i].link}`, {descriptionLength: 110 });
     const { url, title, description, image } = metadata;
     arr.push({metaUrl:url, metaTitle:title, metaDescription:description, metaImage:image})
-    
     
 }
 const response =[];
@@ -20,9 +24,6 @@ for(let i = 0; i< listPosts.rows.length ; i++){
 response.push({...listPosts.rows[i],...arr[i]});
 
 }
-
-console.log(response);
-
 
        return res.status(200).send(response)
     } catch (error) {
